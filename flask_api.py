749a0d5e1c97e5c2a4d5af19dfd98d45f4b36eb6
webpepp <webgraphix.p@gmail.com>
2025-05-24 16:12:55 +0530

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -1,22 +1,20 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, send_file
 from flask_cors import CORS
-import os
-from test import run_tryon  # assume your test logic is in test.py
+from test import run_tryon
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/tryon', methods=['POST'])
 def tryon():
-    image = request.files['image']
+    person = request.files['image']
     cloth = request.files['cloth']
 
-    image.save('input/person.jpg')
+    person.save('input/person.jpg')
     cloth.save('input/cloth.jpg')
 
-    output_path = run_tryon('input/person.jpg', 'input/cloth.jpg')
-
-    return send_file(output_path, mimetype='image/png')
+    result_path = run_tryon('input/person.jpg', 'input/cloth.jpg')
+    return send_file(result_path, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(port=5000)
